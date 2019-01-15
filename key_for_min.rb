7efc45ff7019a941d218e798935d3dd43a29b94e
Samidh Patel <spatel13456@gmail.com>
2019-01-15 18:17:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  key_for_min_value.each do |names|
-    if names == :adam 
+  key_for_min_value.collect do |names|
+    
 end
